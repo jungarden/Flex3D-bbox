4fa69046d11151dce3ffeb29c68bfe7f9f0f24d2
jungarden <dnjswns3795@gmail.com>
2024-09-02 12:40:43 +0900

    Update making_txtlabels.py

--- multi/making_txtlabels.py
+++ multi/making_txtlabels.py
@@ -5,46 +5,32 @@ import math
 import shutil
 from utils import *
 
-'''#############################################################################################
-<OPTIONS>
-dataset_base_dir : 객체 3D 데이터 경로(라벨링데이터, 원천데이터 폴더를 포함)]
-data : 객체 3D 대상명
-train_ratio : train/test dataset의 비율
-
-
-<OUTPUT>
-LINEMOD 데이터 구성을 위해, 다음 파일들을 생성합니다.
-- test.txt
-- train,txt
-- [폴더명].data
-##############################################################################################'''
-
-############# 1. AIHUB 데이터셋
+############# 1. AIHUB dataset
  
-dataset_base_dir="/data/yolo3d/YOLO3D/3dhub/MISO/dataset"
+dataset_base_dir = "/path/to/dataset"
 data = "glove00"
 
 # ratio
-train_ratio = 0.8  # test_ratio = 1 -train_ratio
+train_ratio = 0.8  # test_ratio = 1 - train_ratio
 
-# 원본 이미지 사이즈
-img_width,img_height = '1280','720' #예) AIHUB 데이터셋
+# Original image size
+img_width, img_height = '1280', '720'  # e.g., AIHUB dataset
 
-# 원천데이터
-origin_data_dir = dataset_base_dir + "/" + data + "/" + data + ".원천데이터"
+# Source data
+origin_data_dir = dataset_base_dir + "/" + data + "/" + data + ".SourceData"
 origin_image_dir = origin_data_dir + "/" + data + '.Images'
 
-# 라벨링데이터
-labeling_dir = dataset_base_dir + "/" + data + "/" + data + ".라벨링데이터"
+# Labeling data
+labeling_dir = dataset_base_dir + "/" + data + "/" + data + ".LabelingData"
 labeling_threed_json_dir = labeling_dir + "/" + data + ".3D_json"
 
-# ############# 2. Custom 데이터셋
+# ############# 2. Custom dataset
 # data = "custom_dataset"
 
 # # ratio
-# train_ratio = 0.8 # test_ratio = 1 -train_ratio
+# train_ratio = 0.8  # test_ratio = 1 - train_ratio
 
-# # 원천데이터
+# # Source data
 # origin_dir = dataset_base_dir + "/" + data
 # origin_image_dir = origin_dir
 # labeling_threed_json_dir = origin_dir
@@ -98,13 +84,13 @@ class DataProperty:
 if not (os.path.isdir("data")):
     os.makedirs(os.path.join("data"))
 
-if not (os.path.isdir(os.path.join("data",data))):
-    os.makedirs(os.path.join("data",data))
+if not (os.path.isdir(os.path.join("data", data))):
+    os.makedirs(os.path.join("data", data))
 
-dataProperty = DataProperty();
+dataProperty = DataProperty()
 dataProperty.setTrain('data/' + data + '/train.txt')
 dataProperty.setValid('data/' + data + '/test.txt')
-dataProperty.setBackup('backup/' + data )
+dataProperty.setBackup('backup/' + data)
 dataProperty.setName('glove')
 
 dataProperty.setGpus("0,1")
@@ -123,7 +109,7 @@ with open(os.path.join('data', data + ".data"), 'w') as file:
 
 name_list = os.listdir(labeling_threed_json_dir)
 
-labels_txt_data_dir = os.path.join('data', data, 'labels');
+labels_txt_data_dir = os.path.join('data', data, 'labels')
 if not (os.path.isdir(labels_txt_data_dir)):
     os.makedirs(os.path.join(labels_txt_data_dir))
 
@@ -131,12 +117,12 @@ width = float(dataProperty.getWidth())
 height = float(dataProperty.getHeight())
 
 for file_name in name_list:
-    # 파일 확장자가 .json인 파일만 처리
+    # Process only files with .json extension
     if file_name.endswith('.json'):
         file_path = os.path.join(labeling_threed_json_dir, file_name)
         with open(file_path, 'r') as f:
             data2 = json.load(f)
-        # 'labelingInfo'와 관련된 예외 처리 추가
+        # Add exception handling related to 'labelingInfo'
             if 'labelingInfo' in data2 and '3DBox' in data2['labelingInfo'][0] and 'location' in data2['labelingInfo'][0]['3DBox']:
                 location = data2['labelingInfo'][0]['3DBox']['location'][0]
                 k = open(labels_txt_data_dir + '/' + file_name[-18:-5] + '.txt', 'w')
